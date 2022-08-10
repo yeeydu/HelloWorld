01f25d278f2274d85af0d44301bd38ecbe70c0d7
yeeyson.duarte <yeeyson.duarte@DEVSCOPE335>
2022-08-10 17:10:13 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            string value = $"{"\n"}Hello, {name}, on {currentDate:d} at {currentDate:t}!";
+            Console.WriteLine(value);
+            Console.Write($" \n Press any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
